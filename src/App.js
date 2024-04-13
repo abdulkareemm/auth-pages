4b8de42cd66e1336c2bc751d2b811d5db0ec262d
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-13 12:54:11 +0300

    fix url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes as Router } from "react-router-dom";
+import { Route, Routes as Router,Navigate } from "react-router-dom";
 import {LoginPage,RegisterPage,ForgetPasswordPage} from "./pages/auth"
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Route path="/" element={<Navigate to="/auth/login"/>}/>
         <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/auth/register" element={<RegisterPage />} />
         <Route path="/auth/forget-password" element={<ForgetPasswordPage />} />
